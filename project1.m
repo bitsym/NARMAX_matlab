f448f3be0a826cf18d7361f9b5d337021efaad15
wily860426 <56736490+wily860426@users.noreply.github.com>
2019-10-25 18:28:45 -0400

    Update project1.m

--- project1.m
+++ project1.m
@@ -3,17 +3,6 @@ close all;
 
 addpath('narmaxutils');
 
-%% Generate test data from model
-% N = 300;
-% e = 0.1*randn(N,1);
-% y = zeros(N,1);
-% u = 2*rand(N,1) - 1;
-
-% y(2) = -0.605*y(1) + 0.588*u(1) + e(2);
-% for k = 3:N
-%     y(k) = -0.605*y(k-1) - 0.163*y(k-2)^2 + 0.588*u(k-1) - 0.240*u(k-2) - 0.4*e(k-1) + e(k);
-% end
-
 %% read data
 m = readtable('sample.csv','ReadVariableNames',true,'Delimiter',',');
 
@@ -40,7 +29,7 @@ nmodel = narmax(y, u);
 
 %% Configure and invoke frols algorithm passing NARMAX model
 ny = 2;
-nu = 0;
+nu = 1;
 ne = 2;
 nl = 2;
 nterms = [4 1];
